e1b64e34e414ed79b5bd8c7f85c9bc900b1b73e4
hyoyeon <dlgydus099@gmail.com>
2022-09-19 14:34:22 +0900

    부트스트랩 연결후 test api

---
+++ restaurant/src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
@@ -0,0 +1,7 @@
+package com.example.restaurant.wishlist.repository;
+
+import org.springframework.stereotype.Repository;
+/*
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+}*/
